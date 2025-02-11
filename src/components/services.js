8a5ae4c1a3b4368d01fbc2debe6d0c7700230010
elnakouzimohamed <nakouzim2004@gmail.com>
2025-02-11 15:37:44 +0200

    e

--- src/components/services.js
+++ src/components/services.js
@@ -2,7 +2,7 @@ import '../styles/Services.css';
 
 const Services = () => {
     const services = [
-        { id: 1, title: 'Service 01', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti aspernatur nihil, tenetur tempora labore!', image: '/service.jpg' },
+        { id: 1, title: 'Service 01', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti aspernatur nihil, tenetur tempora labore!', image: '../service.jpg' },
         { id: 2, title: 'Service 02', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti aspernatur nihil, tenetur tempora labore!', image: '/service.jpg' },
         { id: 3, title: 'Service 03', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti aspernatur nihil, tenetur tempora labore!', image: '/service.jpg' }
     ];
